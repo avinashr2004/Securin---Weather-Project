3609c6eb64b7960d7cc4bacaf1309cdfbc6d7635
Avinash <avinashr.aids2022@citchennai.net>
2025-11-05 22:05:56 +0530

    test commit

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 # Get the exact location of THIS file on your computer
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Force the database to be created in the same folder
+# Force the database to be created in the same folde
 DB_PATH = os.path.join(BASE_DIR, "weather.db")
 DATABASE_URL = f"sqlite:///{DB_PATH}"
 
